d5e431f092f43d36f0ecd86219f31dcec7e6ada5
Rachel Collingridge <rachel@powershop.co.nz>
2015-04-21 09:36:52 +1200

    started on creation of billing page

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -1,9 +1,5 @@
 class BillingController < ApplicationController
-  def balance_email
-    # TODO mail a real balance
-    @person = Person.find(params[:id])
-
-    PersonMailer.balance_email(@person).deliver
-    redirect_to(:back)
-  end
+ def index
+  
+ end
 end
